7281da77a90fa945e4be09103b24098204a4698b
ten10ten10 <wdyhap@gmail.com>
2025-09-03 23:55:00 +0900

    main

---
+++ cmd/main/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/ten10ten10/go-mysql-practice/pkg/routes"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
+}
